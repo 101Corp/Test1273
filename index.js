1327dc9a1790ab0ed6340ea1cf76c546343a3eb9
101Corp <81651576+101Corp@users.noreply.github.com>
2024-06-19 17:25:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+const express = require('express');
+const app = express();
+
 app.get('/', (req, res) => {
     const ip = req.ip || req.connection.remoteAddress;
-  console.log(ip)
     res.send(`Your IP address is: ${ip}`);
+});
+
+app.listen(8080, () => {
+    console.log('Server running at http://127.0.0.1:8080/');
 });
